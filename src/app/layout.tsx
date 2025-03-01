dbbbf9ef6c9461c529632377a7860025742cdcd1
siraken <shirasawa.kento@gmail.com>
2025-03-01 16:52:41 +0900

    Configure Noto Sans JP font for site-wide typography

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import './globals.css';
 const notoSansJP = Noto_Sans_JP({
   variable: '--font-noto-sans-jp',
   subsets: ['latin'],
+  weight: ['400', '500', '700'],
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
@@ -35,8 +37,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja">
-      <body className={`${notoSansJP.variable} antialiased`}>{children}</body>
+    <html lang="ja" className={`${notoSansJP.variable}`}>
+      <body className={'font-base antialiased'}>{children}</body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
       },
+      fontFamily: {
+        base: ['var(--font-noto-sans-jp)'],
+      },
     },
   },
   plugins: [],
