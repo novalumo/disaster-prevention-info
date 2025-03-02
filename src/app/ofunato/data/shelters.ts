d94f7790c94476462ca05f43a89f38c4872f8e04
siraken <shirasawa.kento@gmail.com>
2025-03-02 14:23:48 +0900

    refactor: update shelter data with precise coordinates and Google Maps app links

--- src/app/ofunato/data/shelters.ts
+++ src/app/ofunato/data/shelters.ts
@@ -1,4 +1,4 @@
-import { Shelter } from '../ShelterInfoCard';
+import type { Shelter } from '../ShelterInfoCard';
 
 export const shelters: Shelter[] = [
   {
@@ -6,120 +6,120 @@ export const shelters: Shelter[] = [
     name: '介護老人福祉施設「ひまわり」',
     address: '大船渡市大船渡町字山馬越１９７',
     type: '福祉施設',
-    mapUrl: 'https://maps.google.com/?q=大船渡市大船渡町字山馬越１９７',
+    mapUrl: 'https://maps.app.goo.gl/vQJvdPBn7z3xdCtV8',
     position: {
-      lat: 39.0626,
-      lng: 141.7207
-    }
+      lat: 39.07038620640366,
+      lng: 141.71080688696222,
+    },
   },
   {
     id: 'seijin',
     name: '特別養護老人ホーム　成仁ハウス百年の里',
     address: '大船渡市立根町字宮田９－１',
     type: '福祉施設',
-    mapUrl: 'https://maps.google.com/?q=大船渡市立根町字宮田９－１',
+    mapUrl: 'https://maps.app.goo.gl/naCuVamT1Hox63ea8',
     position: {
-      lat: 39.0731,
-      lng: 141.7325
-    }
+      lat: 39.1004563451666,
+      lng: 141.71063826565236,
+    },
   },
   {
     id: 'ofunato-jhs',
     name: '大船渡中学校（屋内体育館）',
     address: '大船渡市大船渡町字永沢94番地1',
     type: '学校施設',
-    mapUrl: 'https://maps.google.com/?q=大船渡市大船渡町字永沢94番地1',
+    mapUrl: 'https://maps.app.goo.gl/2uUnSf3tpvTqHFdKA',
     position: {
-      lat: 39.0663,
-      lng: 141.7179
-    }
+      lat: 39.05331115505726,
+      lng: 141.71739516675112,
+    },
   },
   {
     id: 'inokawa-es',
     name: '猪川小学校（屋内体育館）',
     address: '大船渡市猪川町字轆轤石23番地',
     type: '学校施設',
-    mapUrl: 'https://maps.google.com/?q=大船渡市猪川町字轆轤石23番地',
+    mapUrl: 'https://maps.app.goo.gl/fm9a4GavRqPENSC68',
     position: {
-      lat: 39.0789,
-      lng: 141.7252
-    }
+      lat: 39.09400432354589,
+      lng: 141.7108146437386,
+    },
   },
   {
     id: 'keisen-en',
     name: '介護老人保健施設「気仙苑」',
     address: '大船渡市大船渡町字山馬越188',
     type: '福祉施設',
-    mapUrl: 'https://maps.google.com/?q=大船渡市大船渡町字山馬越188',
+    mapUrl: 'https://maps.app.goo.gl/6nXHnKujLsjb1GMDA',
     position: {
-      lat: 39.0628,
-      lng: 141.7205
-    }
+      lat: 39.0714848919842,
+      lng: 141.71056206933275,
+    },
   },
   {
     id: 'fukushi-no-sato',
     name: '岩手県立福祉の里センター',
     address: '大船渡市立根町字田ノ上30-20',
     type: '公共施設',
-    mapUrl: 'https://maps.google.com/?q=大船渡市立根町字田ノ上30-20',
+    mapUrl: 'https://maps.app.goo.gl/hwdL58B19Zgn6ZbB6',
     position: {
-      lat: 39.0743,
-      lng: 141.7315
-    }
+      lat: 39.10174558169489,
+      lng: 141.71826824375637,
+    },
   },
   {
     id: 'sanriku-no-sono',
     name: 'さんりくの園',
     address: '大船渡市三陸町越喜来字所通91',
     type: '福祉施設',
-    mapUrl: 'https://maps.google.com/?q=大船渡市三陸町越喜来字所通91',
+    mapUrl: 'https://maps.app.goo.gl/UTXUfx7uhrqGKGUCA',
     position: {
-      lat: 39.1143,
-      lng: 141.8088
-    }
+      lat: 39.12523682778302,
+      lng: 141.80541601234924,
+    },
   },
   {
     id: 'rias-hall',
     name: 'リアスホール',
     address: '大船渡市盛町字下舘下18番地1',
     type: '公共施設',
-    mapUrl: 'https://maps.google.com/?q=大船渡市盛町字下舘下18番地1',
+    mapUrl: 'https://maps.app.goo.gl/2Wx3VpC1tAy97utm9',
     position: {
-      lat: 39.0816,
-      lng: 141.7088
-    }
+      lat: 39.07787558432854,
+      lng: 141.7103936501581,
+    },
   },
   {
     id: 'ofunato-1st-jhs',
     name: '大船渡第一中学校（屋内体育館）',
     address: '大船渡市立根町字宮田86番地',
     type: '学校施設',
-    mapUrl: 'https://maps.google.com/?q=大船渡市立根町字宮田86番地',
+    mapUrl: 'https://maps.app.goo.gl/V9fLBW4iREBSHvq67',
     position: {
-      lat: 39.0731,
-      lng: 141.7325
-    }
+      lat: 39.102506510054866,
+      lng: 141.71379064312876,
+    },
   },
   {
     id: 'sanriku-center',
     name: '三陸公民館',
     address: '大船渡市三陸町越喜来字前田36－1',
     type: '公共施設',
-    mapUrl: 'https://maps.google.com/?q=大船渡市三陸町越喜来字前田36－1',
+    mapUrl: 'https://maps.app.goo.gl/HNHMABoUipiZLLYZ9',
     position: {
-      lat: 39.1143,
-      lng: 141.8088
-    }
+      lat: 39.12016227244793,
+      lng: 141.8126239172137,
+    },
   },
   {
     id: 'yoshikirai-es',
     name: '越喜来小学校（屋内体育館）',
     address: '大船渡市三陸町越喜来字小出24-4',
     type: '学校施設',
-    mapUrl: 'https://maps.google.com/?q=大船渡市三陸町越喜来字小出24-4',
+    mapUrl: 'https://maps.app.goo.gl/2PjYDFFQkcXkxqed8',
     position: {
-      lat: 39.1143,
-      lng: 141.8088
-    }
+      lat: 39.126079856565866,
+      lng: 141.81001699967243,
+    },
   },
 ];
