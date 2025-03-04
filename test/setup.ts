831ea3434b4417d3a7163383377250ca4ddf186f
siraken <shirasawa.kento@gmail.com>
2025-03-04 20:46:20 +0900

    chore: Lint and formatting cleanup across project configs

--- test/setup.ts
+++ test/setup.ts
@@ -1,10 +1,10 @@
-import '@testing-library/jest-dom'
-import { expect, afterEach } from 'vitest'
-import { cleanup } from '@testing-library/react'
-import matchers from '@testing-library/jest-dom/matchers'
+import '@testing-library/jest-dom';
+import { expect, afterEach } from 'vitest';
+import { cleanup } from '@testing-library/react';
+import matchers from '@testing-library/jest-dom/matchers';
 
-expect.extend(matchers)
+expect.extend(matchers);
 
 afterEach(() => {
-  cleanup()
-})
+  cleanup();
+});
