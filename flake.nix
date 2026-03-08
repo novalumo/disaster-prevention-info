175a3fb6b8c04e346d1608c7052c9d19f7c076df
Kento Shirasawa <shirasawa@novalumo.com>
2026-03-08 23:07:35 +0900

    chore: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "disaster-prevention-info";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              nodejs_22
+              corepack_22
+            ];
+          };
+        }
+      );
+    };
+}
